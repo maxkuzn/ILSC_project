ac6a96ef21837ec0ac9b6ecdd6d8f21593123013
Alexey Larionov <alxs.larionov@gmail.com>
2020-12-12 02:10:57 +0300

    Fix paths counting to work with any graph (naive)

--- graph/paths_counts_naive.h
+++ graph/paths_counts_naive.h
@@ -3,24 +3,30 @@
 // NOTE(laralex): The output graph has the same base type (like AdjacencyMatrix),
 // but a different Edge type (since we count number of paths, it's an integer)
 template<template<typename> typename GraphT, typename EdgeT>
-GraphT<std::uint32_t> count_paths_of_len(const GraphT<EdgeT>& graph, size_t /*path_len*/) {
-  auto result = GraphT<std::uint32_t>(graph.size());
-  
-  // Just to pass the test
-  result.add_edge(0, 0, 2);
-  result.add_edge(0, 1, 1);
-  result.add_edge(0, 2, 2);
-  result.add_edge(0, 3, 2);
-  
-  result.add_edge(1, 0, 2);
-  result.add_edge(1, 1, 1);
-  result.add_edge(1, 2, 2);
-  result.add_edge(1, 3, 2);
+GraphT<std::uint32_t> count_paths_of_len(const GraphT<EdgeT>& graph, size_t path_len) {
+  auto edges_flags = GraphT<std::uint32_t>(graph.size());
+  for (size_t a = 0; a < graph.size(); ++a){
+    for (size_t b = 0; b < graph.size(); ++b) {
+      if (graph.has_edge(a, b)) {
+        edges_flags.add_edge(a, b, 1);
+      }
+    }
+  }
 
-  result.add_edge(2, 0, 1);
-  result.add_edge(2, 1, 1);
-  result.add_edge(2, 2, 1);
-  result.add_edge(2, 3, 1);
-
+  // naive O(N) multiplication with plenty of hacks to make it work
+  auto result = decltype(edges_flags)(edges_flags);
+  for (size_t p = 0; p < path_len - 1; ++p){
+    auto result_copy = decltype(result)(result);
+    for (size_t i = 0; i < result.size(); ++i){
+      for (size_t j = 0; j < result.size(); ++j) {
+        result.remove_edge(i, j);
+        for (size_t k = 0; k < result.size(); ++k) {
+          result.add_edge(i, j, result(i, j) + result_copy(i, k) * edges_flags(k, j));
+        }
+      }
+    }
+    
+  }
+//  std::cout << result(0, 0) << " " << result(0, 1) << " " << result(0, 2) << " " << result(0, 3) << std::endl;
   return result;
 }
