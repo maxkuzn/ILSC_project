b249c28f3d2c674bee9b87a2990d3f9511f875f6
Alexey Larionov <alxs.larionov@gmail.com>
2020-12-12 16:26:58 +0300

    Paths counts optimization up to O(logP) + test

--- tests/test_paths_counts.cpp
+++ tests/test_paths_counts.cpp
@@ -39,29 +39,60 @@ TYPED_TEST(PathCountsTest, Naive) {
   graph.add_edge(2, 1);
   graph.add_edge(2, 3);
 
-  /* Number of paths of exactly 3 jumps:
-   *  2 1 2 2            
-   *  2 1 2 2     
-   *  1 1 1 1
-   *  0 0 0 0
-   */
-  size_t path_len = 3;
-  auto paths_counts = count_paths_of_len(graph, path_len);
-  auto expected_paths_counts = decltype(paths_counts){ N_NODES };
-  expected_paths_counts.add_edge(0,0,2);
-  expected_paths_counts.add_edge(0,1,1);
-  expected_paths_counts.add_edge(0,2,2);
-  expected_paths_counts.add_edge(0,3,2);
+  {
+    /* Number of paths of exactly 3 jumps:
+    *  2 1 2 2            
+    *  2 1 2 2     
+    *  1 1 1 1
+    *  0 0 0 0
+    */
+    size_t path_len = 3;
+    auto paths_counts = count_paths_of_len(graph, path_len);
+    auto expected_paths_counts = decltype(paths_counts){ N_NODES };
+    expected_paths_counts.add_edge(0,0,2);
+    expected_paths_counts.add_edge(0,1,1);
+    expected_paths_counts.add_edge(0,2,2);
+    expected_paths_counts.add_edge(0,3,2);
 
-  expected_paths_counts.add_edge(1,0,2);
-  expected_paths_counts.add_edge(1,1,1);
-  expected_paths_counts.add_edge(1,2,2);
-  expected_paths_counts.add_edge(1,3,2);
+    expected_paths_counts.add_edge(1,0,2);
+    expected_paths_counts.add_edge(1,1,1);
+    expected_paths_counts.add_edge(1,2,2);
+    expected_paths_counts.add_edge(1,3,2);
 
-  expected_paths_counts.add_edge(2,0,1);
-  expected_paths_counts.add_edge(2,1,1);
-  expected_paths_counts.add_edge(2,2,1);
-  expected_paths_counts.add_edge(2,3,1);
+    expected_paths_counts.add_edge(2,0,1);
+    expected_paths_counts.add_edge(2,1,1);
+    expected_paths_counts.add_edge(2,2,1);
+    expected_paths_counts.add_edge(2,3,1);
 
-  ASSERT_TRUE(  paths_counts == expected_paths_counts );
+    ASSERT_TRUE(  paths_counts == expected_paths_counts );
+  }
+  
+    {
+    /* Number of paths of exactly 3 jumps:
+    *  2 1 2 2            
+    *  2 1 2 2     
+    *  1 1 1 1
+    *  0 0 0 0
+    */
+    size_t path_len = 11;
+    auto paths_counts = count_paths_of_len(graph, path_len);
+    auto expected_paths_counts = decltype(paths_counts){ N_NODES };
+    expected_paths_counts.add_edge(0,0,89);
+    expected_paths_counts.add_edge(0,1,55);
+    expected_paths_counts.add_edge(0,2,89);
+    expected_paths_counts.add_edge(0,3,89);
+
+    expected_paths_counts.add_edge(1,0,89);
+    expected_paths_counts.add_edge(1,1,55);
+    expected_paths_counts.add_edge(1,2,89);
+    expected_paths_counts.add_edge(1,3,89);
+
+    expected_paths_counts.add_edge(2,0,55);
+    expected_paths_counts.add_edge(2,1,34);
+    expected_paths_counts.add_edge(2,2,55);
+    expected_paths_counts.add_edge(2,3,55);
+
+    ASSERT_TRUE(  paths_counts == expected_paths_counts );
+  }
+
 }
